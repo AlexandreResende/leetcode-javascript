204704c23b9655e7209b3ff891c65a9c42b80989
AlexandreResende <alexandre.amres@gmail.com>
2019-08-12 08:18:18 -0300

    chore: Implement 595. Big Countries

---
+++ algorithms/595. Big Countries.js
@@ -0,0 +1,7 @@
+/*
+ * This is an SQL exercise where I just need to write the query
+ * to solve it. The exercise is in the following link:
+ * https://leetcode.com/problems/big-countries/
+ * SELECT name from World WHERE area > 3000000 OR population > 25000000;
+ *
+ */
